bc35cea2e25dabaa973be17a9bad61614b56d438
唐国雄 <1593292349@qq.com>
2024-11-30 22:45:02 +0800

    feat(built-in): ObjectSet优化性能

--- front-end/src/built-in/set/ObjectSet.ts
+++ front-end/src/built-in/set/ObjectSet.ts
@@ -35,8 +35,11 @@ export default class ObjectSet implements Set<string>{
 	}
 
 	forEach(callbackfn:(value:string, value2:string, set:Set<string>) => void, thisArg?:any):void{
-		for(const value of this.values()){
-			callbackfn.call(thisArg, value, value, this);
+		//这里追求性能, 冗余代码
+		const keys = Object.keys(this._cache);
+		for(let i = 0; i < keys.length; ++i){
+			const key = keys[i];
+			callbackfn.call(thisArg, key, key, this);
 		}
 	}
 
